52241f61d7bf200976a97fc42ce6c3dbb0fcf4cb
Muhammad Mahad Tariq <mmahad.bese14seecs@nust.edu.pk>
2017-04-19 16:52:16 +0500

    Final commit in lab

--- src/main/java/pk/edu/nust/seecs/gradebook/App.java
+++ src/main/java/pk/edu/nust/seecs/gradebook/App.java
@@ -1,21 +1,39 @@
 package pk.edu.nust.seecs.gradebook;
+
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 
 import pk.edu.nust.seecs.gradebook.dao.CloDao;
 import pk.edu.nust.seecs.gradebook.entity.Clo;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import pk.edu.nust.seecs.gradebook.bo.CloBo;
 
+import pk.edu.nust.seecs.gradebook.dao.*;
+import pk.edu.nust.seecs.gradebook.entity.*;
 /**
- * My main App. 
+ * My main App.
  * <p>
- This executes everything.
+ * This executes everything.
  */
 
 public class App {
 
     public static void main(String[] args) {
+
+        Resource r = new ClassPathResource("Beans.xml");
+        BeanFactory factory = new XmlBeanFactory(r);
+        
+        Clo clo1 = (Clo)factory.getBean("clo");
+        
+        System.out.println(clo1.getName());
+        
         CloDao clodao = new CloDao();
 
         // Add new clo
@@ -25,11 +43,13 @@ public class App {
         clo.setPlo("2");
         clodao.addClo(clo);
 
+        CloBo clobo=new CloBo();
+        clobo.addClo(clo);
+        
         clodao.updateClo(clo);
 
         // Delete an existing clo
         //dao.deleteClo(1);
-
         // Get all clos
         for (Clo iter : clodao.getAllClos()) {
             System.out.println(iter);
@@ -38,7 +58,6 @@ public class App {
         // Get clo by id
         System.out.println(clodao.getCloById(1));
 
-        
     }
 
 }
