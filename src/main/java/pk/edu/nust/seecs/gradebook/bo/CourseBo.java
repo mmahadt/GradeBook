6dc7a4bfc524f0a6bbf29c212adeadabb748e790
Muhammad Mahad Tariq <mmahad.bese14seecs@nust.edu.pk>
2017-04-19 15:15:06 +0500

    Make Business object classes

--- src/main/java/pk/edu/nust/seecs/gradebook/bo/CourseBo.java
+++ src/main/java/pk/edu/nust/seecs/gradebook/bo/CourseBo.java
@@ -5,10 +5,21 @@
  */
 package pk.edu.nust.seecs.gradebook.bo;
 
+import pk.edu.nust.seecs.gradebook.dao.CourseDao;
+
+import pk.edu.nust.seecs.gradebook.entity.Course;
+
 /**
  *
  * @author mmahad.bese14seecs
  */
 public class CourseBo {
-    
+    public void addCourse(Course course){
+        CourseDao dao = new CourseDao();
+        dao.addCourse(course);
+    }
+    public void updateCourse(Course course){
+        CourseDao dao = new CourseDao();
+        dao.updateCourse(course);
+    }
 }
